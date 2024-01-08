d447b0d44b310288db506c3547eb2f915746b952
prathambhalke <prathameshbhalke125@gmail.com>
2024-01-08 23:21:21 +0530

    learned nested Routes in next js

---
+++ src/app/Routing/page.tsx
@@ -0,0 +1,13 @@
+const Routing = () => {
+  return (
+    <div>
+      <h1 className="text-2xl">learn Routing Page</h1>
+      <p>the nested routing </p>
+      <p className="text-lg">
+        - Routing/ - page.tsx - about/ - page.tsx - about/ -page.tsx
+      </p>
+    </div>
+  );
+};
+
+export default Routing;
