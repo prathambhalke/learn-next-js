dc9b3ada277d12b8c8567d55b64bb350588575f0
prathambhalke <prathameshbhalke125@gmail.com>
2024-01-09 11:57:02 +0530

    learned how to create the custom error code

---
+++ src/app/not-found.tsx
@@ -0,0 +1,9 @@
+const notFound = () => {
+  return (
+    <div className="border-2 rounded-lg">
+      <h1>THIS PAGE IS NOT FOUND</h1>
+      <p>this is a custom created not found page</p>
+    </div>
+  );
+};
+export default notFound;
