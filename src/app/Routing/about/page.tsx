c7fcafd3e773945b860ab93fe3cd0104124caf25
prathambhalke <prathameshbhalke125@gmail.com>
2024-01-16 11:43:14 +0530

    Metadata in Next js

--- src/app/Routing/about/page.tsx
+++ src/app/Routing/about/page.tsx
@@ -1,3 +1,8 @@
+import type { Metadata } from "next";
+
+// export const metadata: Metadata = {
+//   title: "code with Pratham",
+// };
 const About = () => {
   return <h1>About Page</h1>;
 };
--- src/app/Routing/dynamicRoute/[productId]/page.tsx
+++ src/app/Routing/dynamicRoute/[productId]/page.tsx
@@ -1,4 +1,25 @@
-const ProductDetails = ({ params }: { params: { productId: string } }) => {
+import { Metadata } from "next";
+
+type Props = {
+  params: {
+    productId: string;
+  };
+};
+// export const metadata = async ({ params }: Props): Promise<Metadata> => {
+//   const title = await new Promise((resolve) => {
+//     setTimeout(() => {
+//       resolve(params.productId);
+//     }, 100);
+//   });
+//   console.log(title);
+//   return { title: ` ${title}` };
+// };
+
+export const metadata = ({ params }: Props): Metadata => {
+  return { title: `${10 || params.productId }` };
+};
+
+const ProductDetails = ({ params }: Props) => {
   return (
     <div>
       <h1 className="text-2xl">Product Detail Id-{params.productId}</h1>
